5ac0a1aa1ab774851dd2845bab7cb98ae6ce9499
Piwer <virtual.country.standart@gmail.com>
2024-05-31 23:04:52 +0200

    Revert "Merge branch 'main' of github.com:ouvcs/ivc"
    This reverts commit 77f62191724cb5219b79baaa32de02129fb84d95, reversing
    changes made to 66a81a7edc586662fb62439fc7ee2d52c9c6812d.

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
-<div class="container">
-    <nav id="navigation">
-      <a href="#" class="logo">Логотип сайта</a>
-      <a aria-label="mobile menu" class="nav-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-      </a>
-        <ul class="menu-left">
-          <li><a href="#about">Пункт меню 1</a></li>
-          <li><a href="#exp">Пункт меню 2</a></li>
-          <li><a href="#skills">Пункт меню 3</a></li>
-          <li><a href="#projects">Пункт меню 4</a></li>
-          <li><a href="#contact">Пункт меню 5</a></li>
-        </ul>
-    </nav>
-  </div>
-</header>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>International Virtual Countries</title>
+    <link rel="stylesheet" href="/css/index.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="logo.png" alt="Логотип">
+            <nav>
+                <ul>
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <?php 
+        echo "если вы хотите хранить код PHP в документах XHTML или XML, то используйте эти теги"; 
+    ?>
+
+    <footer>
+        <div class="container">
+            <p>&copy;</p>
+        </div>
+    </footer>
+</body>
+</html>
