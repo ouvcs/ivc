4ad15b0f185b2fcec4aacb99fbd483a9ab685a9d
Piwer <virtual.country.standart@gmail.com>
2024-05-31 22:48:46 +0200

    PHP

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>International Virtual Countries</title>
+    <link rel="stylesheet" href="/css/index.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img src="logo.png" alt="Логотип">
+            <nav>
+                <ul>
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">О нас</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <?php 
+        echo "если вы хотите хранить код PHP в документах XHTML или XML, то используйте эти теги"; 
+    ?>
+
+    <footer>
+        <div class="container">
+            <p>&copy;</p>
+        </div>
+    </footer>
+</body>
+</html>
